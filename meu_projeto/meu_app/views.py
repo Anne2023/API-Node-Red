92d10f5d50cf996c479313fbad1f52b74a76c1a2
Amábile <amabile.analistadesistemas@gmail.com>
2024-03-13 20:55:15 -0300

    Adiciona API básica usando Django Rest Framework
    - Criado modelo Produto em meu_app/models.py
    - Criado serializador ProdutoSerializer em meu_app/serializers.py
    - Criada visualização ProdutoListCreate em meu_app/views.py para listar e criar produtos
    - Configuradas URLs em meu_app/urls.py para acessar a visualização ProdutoListCreate
    - Migradas as alterações do modelo Produto
    - Testado o funcionamento da API com ações de CRUD básicas

---
+++ meu_projeto/meu_app/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .models import Produto
+from .serializers import ProdutoSerializer
+
+class ProdutoListCreate(generics.ListCreateAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
